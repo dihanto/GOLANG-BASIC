a565972d218ec5cc0048891fb73d417aac408e48
dihanto <dihanto2306@gmail.com>
2023-03-25 17:47:44 +0700

    first cli app with some minor bug

---
+++ final-basic/go.mod
@@ -0,0 +1,3 @@
+module github.com/dihanto/golang-basic/final-basic
+
+go 1.20
---
+++ final-basic/main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Student struct {
+	Name, Gender, Major string
+	Age                 int
+}
+
+var std []Student
+
+func main() {
+
+	for {
+		if Password() {
+			Input()
+		}
+	}
+}
+
+func menu() int {
+	var input int
+
+	fmt.Println("1. Create Student")
+	fmt.Println("2. Show Students")
+	fmt.Println("3. Exit")
+	fmt.Println("Enter the number to choose menu :")
+
+	fmt.Scan(&input)
+
+	return input
+}
+
+func Password() bool {
+	var password string
+
+	fmt.Println("Please enter the password to use this app")
+	fmt.Scan(&password)
+	return password == "jangkrik"
+}
+func Input() {
+	for {
+		input := menu()
+		if input == 1 {
+			var name, major, gender string
+
+			name = strings.ToUpper(InputString("What is the name of student?"))
+			age, _ := strconv.Atoi(InputInt("What is the age of student?"))
+			aged := int(age)
+			major = strings.ToUpper(InputString("What is the major of student?"))
+			gender = GenderInput()
+
+			student := Student{
+				Name:   name,
+				Age:    aged,
+				Major:  major,
+				Gender: gender,
+			}
+			std = append(std, student)
+			// std = append(std, Student{Name: name, Age: age, Major: major, Gender: gender})
+		} else if input == 2 {
+			if len(std) == 0 {
+				fmt.Println("No students found")
+			} else {
+				for i := 0; i < len(std); i++ {
+					fmt.Printf("Name : %s\nAge : %d\nMajor : %s\nGender : %s\n", std[i].Name, std[i].Age, std[i].Major, std[i].Gender)
+					fmt.Println("-------------------")
+				}
+			}
+		} else if input == 3 {
+			os.Exit(1)
+		}
+	}
+}
+func InputString(msg string) (result string) {
+
+	for {
+		var input string
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print(msg)
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Println("Error reading input:", err)
+		}
+		input = strings.TrimSpace(input)
+
+		if len(input) == 0 {
+			fmt.Println("input is empty, please type something")
+			continue
+		}
+
+		_, err = strconv.Atoi(input)
+		if err == nil {
+			fmt.Println("input not valid, please read the instruction carefully.")
+			continue
+		} else if input == "true" {
+			fmt.Println("input not valid, please read the instruction carefully.")
+			continue
+		} else if input == "false" {
+			fmt.Println("input not valid, please read the instruction carefully")
+			continue
+		}
+
+		result = input
+		break
+	}
+	return result
+}
+func InputInt(msg string) string {
+	var input string
+	// var err error
+	for {
+		fmt.Println(msg)
+		fmt.Scan(&input)
+
+		_, err := strconv.ParseInt(input, 10, 32)
+
+		if err != nil {
+			fmt.Println("input not valid, please enter the age of student")
+			continue
+		}
+		break
+	}
+	return input
+}
+
+func GenderInput() string {
+	for {
+		data := InputString("What is the gender of student :")
+		if data == "male" {
+			return fmt.Sprintln("Male")
+		} else if data == "female" {
+			return fmt.Sprintln("Female")
+		} else {
+			return "Please type male/female"
+		}
+	}
+}
+
+// func StringChecker(input interface{}) bool {
+// 	return reflect.TypeOf(input).Kind() != reflect.String
+// }
